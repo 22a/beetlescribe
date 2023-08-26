ec2f35e31e2d7b3b35883e0b6ac291b4e9d7bce7
Peter Meehan <peter.meehan@intercom.io>
2023-08-26 17:49:19 +0100

    yoink schema logic from github.com/BlueWinds/bsd-schema (#2)

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,28 +1,14 @@
 import EditorScreen from './components/editor/editor'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getEntireBackend } from './lib/backend'
 
-const index = await (
-  await fetch(
-    'https://github.com/BSData/gallery/releases/latest/download/bsdata.catpkg-gallery.json'
-  )
-).json()
-const targetRepoInfo = index.repositories.find((r) => r.name === 'wh40k-10e')
-const repoContents = await (await fetch(targetRepoInfo.repositoryUrl)).json()
-const armies = repoContents.repositoryFiles
-const singleArmy = armies[0]
-const armyCatUrl = singleArmy.githubUrl
-  .replace('https://github.com', 'https://raw.githubusercontent.com')
-  .replace('/blob/', '/')
-const armyCat = await (await fetch(armyCatUrl)).text()
-const parser = new DOMParser();
-const armyCatDoc = parser.parseFromString(armyCat, "application/xml");
-// console.log({ repoContents, armies, singleArmy, armyCat })
-console.log(armyCatDoc)
+const data = await getEntireBackend()
+console.log(data)
 
 function App(): JSX.Element {
   return (
-      <EditorScreen></EditorScreen>
-    )
+    <EditorScreen></EditorScreen>
+  )
 }
 
 export default App
