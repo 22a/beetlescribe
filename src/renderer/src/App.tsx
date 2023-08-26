589570a09c63949f0adb732b8a21935280d5a8ff
Peter Meehan <peter.meehan@intercom.io>
2023-08-26 14:14:19 +0100

    implement POC fetching of army catalogues

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,24 @@
 import Versions from './components/Versions'
 import icons from './assets/icons.svg'
 
+const index = await (
+  await fetch(
+    'https://github.com/BSData/gallery/releases/latest/download/bsdata.catpkg-gallery.json'
+  )
+).json()
+const targetRepoInfo = index.repositories.find((r) => r.name === 'wh40k-10e')
+const repoContents = await (await fetch(targetRepoInfo.repositoryUrl)).json()
+const armies = repoContents.repositoryFiles
+const singleArmy = armies[0]
+const armyCatUrl = singleArmy.githubUrl
+  .replace('https://github.com', 'https://raw.githubusercontent.com')
+  .replace('/blob/', '/')
+const armyCat = await (await fetch(armyCatUrl)).text()
+const parser = new DOMParser();
+const armyCatDoc = parser.parseFromString(armyCat, "application/xml");
+// console.log({ repoContents, armies, singleArmy, armyCat })
+console.log(armyCatDoc)
+
 function App(): JSX.Element {
   return (
     <div className="container">
