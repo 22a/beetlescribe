e2c6dd4be0b033e06da414dfeb50534fdd3c9162
Peter Meehan <peter.meehan@intercom.io>
2023-08-26 17:42:10 +0100

    yoink schema logic from github.com/BlueWinds/bsd-schema

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,23 +1,9 @@
 import Versions from './components/Versions'
 import icons from './assets/icons.svg'
+import { getEntireBackend } from './lib/backend'
 
-const index = await (
-  await fetch(
-    'https://github.com/BSData/gallery/releases/latest/download/bsdata.catpkg-gallery.json'
-  )
-).json()
-const targetRepoInfo = index.repositories.find((r) => r.name === 'wh40k-10e')
-const repoContents = await (await fetch(targetRepoInfo.repositoryUrl)).json()
-const armies = repoContents.repositoryFiles
-const singleArmy = armies[0]
-const armyCatUrl = singleArmy.githubUrl
-  .replace('https://github.com', 'https://raw.githubusercontent.com')
-  .replace('/blob/', '/')
-const armyCat = await (await fetch(armyCatUrl)).text()
-const parser = new DOMParser();
-const armyCatDoc = parser.parseFromString(armyCat, "application/xml");
-// console.log({ repoContents, armies, singleArmy, armyCat })
-console.log(armyCatDoc)
+const data = await getEntireBackend()
+console.log(data)
 
 function App(): JSX.Element {
   return (
